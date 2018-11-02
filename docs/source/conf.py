e3412c3689fedf3a0bf118903c77f16f797cdc39
Yuki Hayashi <yuki@alpacadb.com>
2018-11-03 08:44:13 +0900

    Enable napoleon plugin
    This is to enable google style docstring in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,12 @@ release = "0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.todo"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+    "sphinx.ext.napoleon",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. src documentation master file, created by
-   sphinx-quickstart on Sat Nov  3 07:51:22 2018.
+   sphinx-quickstart on Sat Nov  3 08:39:38 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
