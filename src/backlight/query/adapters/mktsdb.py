4a4ca02aa0c720bbad566bbcd76689b20afc8806
Yuki Hayashi <yuki@alpacadb.com>
2018-11-26 10:01:38 +0000

    Apply black

--- src/backlight/query/adapters/mktsdb.py
+++ src/backlight/query/adapters/mktsdb.py
@@ -12,18 +12,20 @@ DEFAULT_RETRIES = 100
 class MarketstoreAdapter(DataSourceAdapter):
     """Data source adapter for Marketstore"""
 
-    def __init__(
-        self,
-        url: str,
-    ) -> None:
+    def __init__(self, url: str) -> None:
         o = urllib.parse.urlparse(url)
         assert o.scheme == "mktsdb"
         self._cli = client.Client(host=o.hostname, port=o.port)
 
     def query(
-            self, symbol: str, start_dt: pd.Timestamp, end_dt: pd.Timestamp,
-            timeframe: str = "1Min") -> pd.DataFrame:
+        self,
+        symbol: str,
+        start_dt: pd.Timestamp,
+        end_dt: pd.Timestamp,
+        timeframe: str = "1Min",
+    ) -> pd.DataFrame:
 
         ret = self._cli.query(
-            symbol=symbol, timeframe=timeframe, start_dt=start_dt, end_dt=end_dt)
+            symbol=symbol, timeframe=timeframe, start_dt=start_dt, end_dt=end_dt
+        )
         return ret.sort_index()
