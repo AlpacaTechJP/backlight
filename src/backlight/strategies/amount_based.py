8dfb6dcdb81e6fd8ab5105261037de6bd0b84c78
keisuke-umezawa <keisuke.umezawa@gmail.com>
2018-11-28 17:13:52 +0900

    Implement entry and exit

--- src/backlight/strategies/amount_based.py
+++ src/backlight/strategies/amount_based.py
@@ -27,6 +27,46 @@ def direction_based_trades(
     return t
 
 
+def _no_condition(df: pd.DataFrame):
+    return pd.Series(index=df.index, data=False)
+
+
+def entry_exit_trades(
+    mkt: MarketData,
+    sig: Signal,
+    direction_action_dict: dict,
+    max_holding_time: pd.Timedelta,
+    exit_condition=_no_condition,
+) -> Trades:
+    """
+    """
+    df = pd.concat([mkt, sig], axis=1, join="inner")
+    df.loc[:, "amount"] = 0.0
+    for idx, row in df.iterrows():
+        action = direction_action_dict[row["pred"]]
+        amount = action.act_on_amount
+        df[idx, "amount"] += amount
+
+        df_to_max_holding_time = df[
+            (idx <= df.index) & (df.index <= idx + max_holding_time)
+        ]
+        exit_indices = df_to_max_holding_time[
+            exit_condition(df_to_max_holding_time)
+        ].index
+        if exit_indices.empty():
+            exit_index = df_to_max_holding_time.index[-1]
+        else:
+            exit_index = exit_indices[0]
+        df[exit_index, "amount"] -= amount
+
+    # Order of concat is important, if mkt in front of trades, will have error
+    # since it will use the type of mkt(MarketData) to concat, but trades is
+    # normal DataFrame
+    t = Trades(df)
+    t.symbol = sig.symbol
+    return t
+
+
 def only_take_long(mkt: MarketData, sig: Signal) -> Trades:
     direction_action_dict = {
         TernaryDirection.UP: Action.TakeLong,
