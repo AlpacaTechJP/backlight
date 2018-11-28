58f520308a9912c3149ef3dd1f2c48d90797a51f
keisuke umezawa <keisuke-umezawa@users.noreply.github.com>
2018-11-28 15:32:57 +0900

    Make MidMarketData and AskBidMarketData (#32)
    * Add MidMarketData and AskBidMarketData
    
    * Rename test

--- src/backlight/datasource/marketdata.py
+++ src/backlight/datasource/marketdata.py
@@ -5,15 +5,12 @@ from typing import Type
 class MarketData(pd.DataFrame):
     """MarketData container which inherits pd.DataFrame."""
 
-    _metadata = ["symbol"]
+    _metadata = ["symbol", "_target_columns"]
 
-    @property
-    def mid(self) -> pd.Series:
-        """Series: mid price"""
-        if "mid" in self.columns:
-            return self["mid"]
-        else:
-            return (self.ask + self.bid) / 2
+    def reset_cols(self) -> None:
+        for col in self.columns:
+            if col not in self._target_columns:
+                self.drop(col, axis=1, inplace=True)
 
     @property
     def _constructor(self) -> Type["MarketData"]:
@@ -26,3 +23,31 @@ class MarketData(pd.DataFrame):
     @property
     def end_dt(self) -> pd.Timestamp:
         return self.index[-1]
+
+
+class MidMarketData(MarketData):
+
+    _target_columns = ["mid"]
+
+    @property
+    def mid(self) -> pd.Series:
+        """Series: mid price"""
+        return self["mid"]
+
+    @property
+    def _constructor(self) -> Type["MidMarketData"]:
+        return MidMarketData
+
+
+class AskBidMarketData(MarketData):
+
+    _target_columns = ["ask", "bid"]
+
+    @property
+    def mid(self) -> pd.Series:
+        """Series: mid price"""
+        return (self.ask + self.bid) / 2.0
+
+    @property
+    def _constructor(self) -> Type["AskBidMarketData"]:
+        return AskBidMarketData
--- tests/datasource/test_marketdata.py
+++ tests/datasource/test_marketdata.py
@@ -1,23 +1,35 @@
 from backlight.datasource import marketdata as module
-from unittest import mock
 import pandas as pd
-import os
 
 
-@mock.patch.dict(os.environ, {"TICK_MARKETSTORE_HOST": "8888"})
 def test_MarketData():
+    start_dt = pd.Timestamp("2018-06-06")
+    end_dt = pd.Timestamp("2018-06-08")
+    df = pd.DataFrame(
+        index=pd.date_range(start=start_dt, periods=3),
+        data=[[0, 2], [2, 4], [4, 6]],
+        columns=["ask", "bid"],
+    )
+    md = module.MarketData(df)
+    assert md.start_dt == start_dt
+    assert md.end_dt == end_dt
+
+
+def test_AskBidMarketData():
     df = pd.DataFrame(
         index=pd.date_range(start="2018-06-06", periods=3),
         data=[[0, 2], [2, 4], [4, 6]],
         columns=["ask", "bid"],
     )
-    md = module.MarketData(df)
+    md = module.AskBidMarketData(df)
     assert all(md.mid.values == [1, 3, 5])
 
+
+def test_MidMarketData():
     df = pd.DataFrame(
         index=pd.date_range(start="2018-06-06", periods=3),
         data=[0, 2, 6],
         columns=["mid"],
     )
-    md = module.MarketData(df)
+    md = module.MidMarketData(df)
     assert all(md.mid.values == [0, 2, 6])
