745f131c9fecd1ffe5059ed74fac0c5eb1976a77
keisuke-umezawa <keisuke.umezawa@gmail.com>
2018-11-30 18:50:49 +0900

    Implement pl metrics

--- src/backlight/metrics/pl.py
+++ src/backlight/metrics/pl.py
@@ -7,29 +7,32 @@ def _sum(a: pd.Series) -> float:
     return a.sum() if len(a) != 0 else 0.0
 
 
-def calc_position_performance(positions: Positions):
-    """Evaluate the pl perfomance of positions"""
+def _pl(positions: Positions) -> pd.Series:
     next_price = positions.price.shift(periods=-1)
     price_diff = next_price - positions.price
     pl = (price_diff * positions.amount).shift(periods=1)[1:]  # drop first nan
+    return pl.rename("pl")
 
-    previous_amount = positions.amount.shift(periods=1)
-    amount_diff = (positions.amount - previous_amount)[1:]  # drop first nan
+
+def _trade_amount(amount: pd.Series) -> pd.Series:
+    previous_amount = amount.shift(periods=1)
+    amount_diff = (amount - previous_amount)[1:]  # drop first nan
+    return _sum(amount_diff.abs())
+
+
+def calc_position_performance(positions: Positions) -> pd.DataFrame:
+    """Evaluate the pl perfomance of positions"""
+    pl = _pl(positions)
+    trade_amount = _trade_amount(positions.amount)
 
     total_pl = _sum(pl)
-    trade_amount = _sum(amount_diff.abs())
     average_pl = total_pl / trade_amount
 
     m = pd.DataFrame.from_records(
-        [
-            ("avg_pl", average_pl),
-            ("total_pl", total_pl),
-            ("trade_amount", trade_amount),
-        ]
+        [("avg_pl", average_pl), ("total_pl", total_pl), ("trade_amount", trade_amount)]
     ).set_index(0)
 
     del m.index.name
     m.columns = ["metrics"]
 
     return m.T
-
--- tests/metrics/test_pl.py
+++ tests/metrics/test_pl.py
@@ -9,13 +9,16 @@ from backlight.positions.positions import Positions
 def positions():
     symbol = "usdjpy"
     data = [
-        [1.0, 1.0],  # pl = 0.0
-        [-1.0, 2.0],  # pl = -1.0
-        [0.0, 3.0],  # pl = 0.0
-        [2.0, 4.0],  # pl = 2.0
-        [-2.0, 5.0],  # pl = 1.0
-        [0.0, 6.0],  # pl = 0.0
-        [0.0, 7.0],  # pl = 0.0
+        [1.0, 1.0],  # pl = None
+        [-1.0, 2.0],  # pl = 1.0 * (2.0 - 1.0) = 1.0
+        [0.0, 3.0],  # pl = -1.0 * (3.0 - 2.0) = -1.0
+        [2.0, 4.0],  # pl = 0.0 * (4.0 - 3.0) = 0.0
+        [-2.0, 5.0],  # pl = 2.0 * (5.0 - 4.0) = 2.0
+        [0.0, 6.0],  # pl = -2.0 * (6.0 - 5.0) = -2.0
+        [1.0, 7.0],  # pl = 0.0 * (7.0 - 6.0) = 0.0
+        [1.0, 8.0],  # pl = 1.0 * (8.0 - 7.0) = 1.0
+        [2.0, 9.0],  # pl = 1.0 * (9.0 - 8.0) = 1.0
+        [2.0, 9.0],  # pl = 2.0 * (9.0 - 9.0) = 0.0
     ]
     df = pd.DataFrame(
         index=pd.date_range(start="2018-06-06", freq="1min", periods=len(data)),
@@ -27,11 +30,25 @@ def positions():
     return positions
 
 
+def test__pl(positions):
+    expected = pd.Series(
+        data=[1.0, -1.0, 0.0, 2.0, -2.0, 0.0, 1.0, 1.0, 0.0],
+        index=positions.index[1:],
+        name="pl",
+    )
+    assert (module._pl(positions) == expected).all()
+
+
+def test__trade_amount(positions):
+    expected = 13.0
+    assert module._trade_amount(positions.amount) == expected
+
+
 def test_calc_position_performance(positions):
     metrics = module.calc_position_performance(positions)
     expected_total_pl = 2.0
-    expected_trade_amount = 5
+    expected_trade_amount = 13.0
     expected_avg_pl = expected_total_pl / expected_trade_amount
     assert metrics.loc["metrics", "total_pl"] == expected_total_pl
-    assert metrics.loc["metrics", "avg_pl"] == expected_avg_pl
     assert metrics.loc["metrics", "trade_amount"] == expected_trade_amount
+    assert metrics.loc["metrics", "avg_pl"] == expected_avg_pl
