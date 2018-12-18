d6cbccc0330e01b91475fb2e86139fa163c17deb
keisuke-umezawa <keisuke.umezawa@gmail.com>
2018-12-18 15:54:04 +0900

    Add docstring

--- src/backlight/positions/positions.py
+++ src/backlight/positions/positions.py
@@ -7,18 +7,27 @@ from backlight.trades.trades import Trade, Trades
 
 
 class Positions(pd.DataFrame):
-    """Dataframe for Positions.
+    """Positions container which inherits pd.DataFrame.
+
+    They have following columns:
+        - `amount`: Amount of the asset you are holding at that moment.
+        - `price`: Price per unit of the asset at that moment.
+        - `principal`: Principal in your bank account at that moment.
     """
+
+    _metadata = ["symbol"]
 
     _target_columns = ["amount", "price", "principal"]
 
     def reset_cols(self) -> None:
+        """ Keep only _target_columns"""
         for col in self.columns:
             if col not in self._target_columns:
                 self.drop(col, axis=1, inplace=True)
 
     @property
     def value(self) -> pd.Series:
+        """ Series of the position valuation"""
         return self.amount * self.price + self.principal
 
     @property
@@ -30,13 +39,12 @@ def _pricer(trade: Trade, mkt: MarketData, principal: float) -> Positions:
     positions = pd.DataFrame(index=mkt.index)
 
     positions.loc[:, "amount"] = trade.amount.cumsum()
-    positions.loc[:, "amount"] = positions["amount"].ffill()
-
     positions.loc[:, "price"] = mkt.mid
 
     fee = mkt.fee(trade.amount)
     positions.loc[:, "principal"] = -fee.cumsum() + principal
-    positions.loc[:, "principal"] = positions["principal"].ffill()
+
+    positions = positions.ffill()
 
     pos = Positions(positions)
     pos.reset_cols()
@@ -47,12 +55,20 @@ def _pricer(trade: Trade, mkt: MarketData, principal: float) -> Positions:
 def calc_positions(
     trades: Trades, mkt: MarketData, principal: float = 0.0
 ) -> Positions:
+    """Create `Positions` from Trades and MarketData.
+
+    Args:
+        trades: Tuple of trades.
+        mkt: Market data.
+        principal: The initial principal value.
+    """
     trade = flatten(trades)
 
     assert trade.symbol == mkt.symbol
     assert (trade.index.isin(mkt.index)).all()
 
     positions = _pricer(trade, mkt, principal)
+    positions.symbol = trade.symbol
     return positions
 
 
