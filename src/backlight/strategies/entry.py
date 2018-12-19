d5bf94f7480720567daad4ef50748ed5aa4e5ac4
keisuke-umezawa <keisuke.umezawa@gmail.com>
2018-12-19 14:07:21 +0900

    Separete entry and exit strategies

---
+++ src/backlight/strategies/entry.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+from backlight.datasource.marketdata import MarketData
+from backlight.signal.signal import Signal
+from backlight.trades import make_trade
+from backlight.trades.trades import Transaction, Trade, Trades
+from backlight.strategies.common import Action, concat
+
+
+def _entry(amount: float, idx: pd.Timestamp, symbol: str) -> Trade:
+    trade = make_trade(symbol)
+    trade.add(Transaction(timestamp=idx, amount=amount))
+    return trade
+
+
+def direction_based_entry(
+    mkt: MarketData, sig: Signal, direction_action_dict: dict
+) -> Trades:
+    """Take positions.
+
+    Args:
+        mkt: Market data
+        sig: Signal data
+        direction_action_dict: Dictionary from signals to actions
+    Result:
+        Trades
+    """
+    df = concat(mkt, sig)
+
+    trades = ()  # type: Trades
+    for direction, action in direction_action_dict.items():
+        amount = action.act_on_amount()
+        if amount == 0.0:
+            continue
+        target_index = df[df["pred"] == direction.value].index
+        trades += tuple(_entry(amount, idx, df.symbol) for idx in target_index)
+
+    return trades
