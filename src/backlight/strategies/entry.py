f959eed60d8cf0683653358bf7ccd7098f23ae8f
keisuke-umezawa <keisuke.umezawa@gmail.com>
2019-02-01 19:16:59 +0900

    Improve performance of trades

--- src/backlight/strategies/entry.py
+++ src/backlight/strategies/entry.py
@@ -5,14 +5,8 @@ from typing import List
 from backlight.datasource.marketdata import MarketData
 from backlight.signal.signal import Signal
 from backlight.trades import make_trade
-from backlight.trades.trades import Transaction, Trades, make_trades, make_trade
+from backlight.trades.trades import Trades
 from backlight.strategies.common import Action
-
-
-def _entry(amount: float, idx: pd.Timestamp, symbol: str) -> pd.Series:
-    t = Transaction(timestamp=idx, amount=amount)
-    trade = make_trade([t])
-    return trade
 
 
 def direction_based_entry(
@@ -30,12 +24,24 @@ def direction_based_entry(
     assert all([idx in mkt.index for idx in sig.index])
     df = sig
 
-    trades = []  # type: List[pd.Series]
+    trades = []  # type: List[pd.Dataframe]
     for direction, action in direction_action_dict.items():
+
         amount = action.act_on_amount()
         if amount == 0.0:
             continue
-        target_index = df[df["pred"] == direction.value].index
-        trades += [_entry(amount, idx, df.symbol) for idx in target_index]
 
-    return make_trades(df.symbol, trades)
+        trades.append(
+            pd.DataFrame(
+                index=df[df["pred"] == direction.value].index,
+                data=direction.value,
+                columns=["amount"],
+            )
+        )
+
+    df_trades = pd.concat(trades, axis=0).sort_index()
+    df_trades.loc[:, "_id"] = range(len(df_trades.index))
+
+    t = Trades(df_trades)
+    t.symbol = df.symbol
+    return t
