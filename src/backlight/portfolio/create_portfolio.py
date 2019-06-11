fc6d42ab7edb539720e6442d5f546002ec897406
Imad El Hanafi <me@imadelhanafi.com>
2019-06-11 17:02:50 +0900

    Add simple portfolio construction

--- src/backlight/portfolio/create_portfolio.py
+++ src/backlight/portfolio/create_portfolio.py
@@ -4,11 +4,14 @@ from backlight.positions.positions import Positions
 from backlight.trades.trades import Trades
 from backlight.signal.signal import Signal
 from backlight.datasource.marketdata import MarketData
-from backlight.portfolio.portfolio import Portfolio
+from backlight import strategies
+from backlight.positions import calc_positions
+
+from joblib import Parallel, delayed
 
 
 def create_simple_portfolio(
-    mkt: List[MarketData], sig: List[Signals], principal: float
+    mkt: List[MarketData], sig: List[Signal], strategy_name: str, strategy_params: dict, principal: float
 ):
     """
     Create portfolio (as a list of positions) from a list of signals of each asset
@@ -16,8 +19,30 @@ def create_simple_portfolio(
     Args:
         mkt: list of marketdata of each asset
         sig: list of signals to be used to construct the porfolio
+        strategy_name: a simple strategy from module strategies
         principal: initial principal value available for the portfolio
 
     return:
-        Portfolio 
+        Portfolio
     """
+
+    # Load strategy
+    strategy = getattr(strategies, strategy_name)
+    principal_per_asset = principal/len(mkt)
+
+    # check markets and signals given in order
+    for (m,s) in zip(mkt,sig):
+        assert m.symbol==s.symbol
+
+    # Apply strategy on each asset and get list of trades
+
+    trades = Parallel(n_jobs=-1)(
+        [delayed(strategy)(market, asset, **strategy_params) for (asset,market) in zip(sig,mkt)]
+    )
+
+    # Construct positions and return Portfolio
+    positions = Parallel(n_jobs=-1)(
+        [delayed(calc_positions)(trade, market, principal = principal_per_asset) for (trade,market) in zip(trades,mkt)]
+    )
+
+    return Portfolio(positions)
--- src/backlight/portfolio/portfolio.py
+++ src/backlight/portfolio/portfolio.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
+from typing import List
 
 from backlight.positions.positions import Positions
-
+from backlight.datasource.marketdata import MarketData
+from backlight.metrics.position_metrics import calc_pl
 
 class Portfolio:
     """
@@ -12,12 +14,44 @@ class Portfolio:
 
     def __init__(self,positions: List[Positions]):
         self.portfolio = positions
-
+        self.pl = pd.DataFrame()
 
     @property
-    def value(self) -> pd.Series:
-        """ Series of the portfolio valuation"""
+    def value(self) -> pd.DataFrame:
+        """ DataFrame of the portfolio valuation of each asset"""
         for asset_positions in self.portfolio:
-            # Compute PL of positions of each asset
+            #Compute PL of positions of each asset
+            self.pl[asset_positions.symbol] = calc_pl(asset_positions)
+        return self.pl
 
-            # Normalize PL to one asset reference and sum
+    #Normalize PL to one asset reference and sum
+    def normalized_total_pl(self, mkt: List[MarketData], type_asset: str = "stock"):
+        """
+        Normalize PL to one asset reference and sum
+        args:
+            - mkt : list of marketdata for each asset
+            - type_asset: FX or same (same are asset that supposed to have pl in the same currency, like stocks or USDJPY and EURJPY)
+
+            # To add when supporting cross assets
+            - reference : asset of reference
+                          for FX, one should pay attention to the family of asset that
+                          share the same base (for example EURJPY, USDJPY, GBPJPY)
+                          then an auto reference would be JPY
+
+                          in case of cross FX (EURUSD, USDJPY) we should convert USD pl from first asset
+                          to JPY using USDJPY market
+
+        """
+        # If not FX, just add all pl
+        if(type_asset != "FX"):
+            return self.pl.sum(1)
+
+        if(type_asset == "FX"):
+            # check if in case all currecnies have same suffix, use last 3 chars as reference
+            reference_to_check = self.pl.columns[0][-3:]
+
+            if(sum(self.pl.columns.str.endswith(reference_to_check)) == len(self.portfolio)):
+                return self.pl.sum(1)
+            else:
+                # Fix me : add support of different assets
+                raise ValueError('Cross FX is not supported, specify FX with same base  e.g. USDJPY, GBPJPY')
