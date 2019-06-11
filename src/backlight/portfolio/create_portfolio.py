8d2fd759b488de3575218e5b49ac9dcd39a3c6d5
Imad El Hanafi <me@imadelhanafi.com>
2019-06-11 17:45:36 +0900

    format_black

--- src/backlight/portfolio/create_portfolio.py
+++ src/backlight/portfolio/create_portfolio.py
@@ -11,7 +11,11 @@ from joblib import Parallel, delayed
 
 
 def create_simple_portfolio(
-    mkt: List[MarketData], sig: List[Signal], strategy_name: str, strategy_params: dict, principal: float
+    mkt: List[MarketData],
+    sig: List[Signal],
+    strategy_name: str,
+    strategy_params: dict,
+    principal: float,
 ):
     """
     Create portfolio (as a list of positions) from a list of signals of each asset
@@ -28,21 +32,27 @@ def create_simple_portfolio(
 
     # Load strategy
     strategy = getattr(strategies, strategy_name)
-    principal_per_asset = principal/len(mkt)
+    principal_per_asset = principal / len(mkt)
 
     # check markets and signals given in order
-    for (m,s) in zip(mkt,sig):
-        assert m.symbol==s.symbol
+    for (m, s) in zip(mkt, sig):
+        assert m.symbol == s.symbol
 
     # Apply strategy on each asset and get list of trades
 
     trades = Parallel(n_jobs=-1)(
-        [delayed(strategy)(market, asset, **strategy_params) for (asset,market) in zip(sig,mkt)]
+        [
+            delayed(strategy)(market, asset, **strategy_params)
+            for (asset, market) in zip(sig, mkt)
+        ]
     )
 
     # Construct positions and return Portfolio
     positions = Parallel(n_jobs=-1)(
-        [delayed(calc_positions)(trade, market, principal = principal_per_asset) for (trade,market) in zip(trades,mkt)]
+        [
+            delayed(calc_positions)(trade, market, principal=principal_per_asset)
+            for (trade, market) in zip(trades, mkt)
+        ]
     )
 
     return Portfolio(positions)
