3791f42b6d2c6de382c3003dfd4f1df2650af8d7
Imad El Hanafi <me@imadelhanafi.com>
2019-06-20 14:13:55 +0900

    Add unittests

---
+++ tests/portfolio/test_portfolio.py
@@ -0,0 +1,130 @@
+import pytest
+import pandas as pd
+import numpy as np
+from backlight.portfolio.portfolio import construct_portfolio as module
+
+import backlight
+
+from backlight.trades.trades import make_trades
+
+
+@pytest.fixture
+def trades():
+    trades = []
+    index = [
+        "2018-06-06 00:00:00",
+        "2018-06-06 00:01:00",
+        "2018-06-06 00:02:00",
+        "2018-06-06 00:03:00",
+        "2018-06-06 00:03:00",
+        "2018-06-06 00:04:00 ",
+        "2018-06-06 00:05:00",
+        "2018-06-06 00:05:00",
+        "2018-06-06 00:06:00 ",
+        "2018-06-06 00:06:00 ",
+        "2018-06-06 00:07:00 ",
+        "2018-06-06 00:08:00 ",
+        "2018-06-06 00:09:00 ",
+        "2018-06-06 00:09:00 ",
+    ]
+
+    trade = pd.Series(
+        index=pd.to_datetime(index),
+        data=[1, -1, -1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, -1],
+        name="amount",
+    )
+    ids = [0, 1, 0, 1, 2, 3, 2, 4, 3, 5, 4, 5, 6, 6]
+
+    trades.append(make_trades("usdjpy", [trade], [ids]))
+    trades.append(make_trades("eurjpy", [trade], [ids]))
+    return trades
+
+
+@pytest.fixture
+def markets():
+    markets = []
+    symbol = "usdjpy"
+    periods = 10
+    df = pd.DataFrame(
+        index=pd.date_range(start="2018-06-06", freq="1min", periods=periods),
+        data=np.arange(periods)[:, None],
+        columns=["mid"],
+    )
+    markets.append(backlight.datasource.from_dataframe(df, symbol))
+
+    symbol = "eurjpy"
+    periods = 10
+    df = pd.DataFrame(
+        index=pd.date_range(start="2018-06-06", freq="1min", periods=periods),
+        data=10 - np.arange(periods)[:, None],
+        columns=["mid"],
+    )
+    markets.append(backlight.datasource.from_dataframe(df, symbol))
+    return markets
+
+
+@pytest.fixture
+def principal():
+    return [10, 10]
+
+
+@pytest.fixture
+def lot_size():
+    return [2, 2]
+
+
+def test_construct_portfolio(trades, markets, principal, lot_size):
+    portfolio = module(trades, markets, principal, lot_size)
+
+    index = [
+        "2018-06-05 23:59:00",
+        "2018-06-06 00:00:00",
+        "2018-06-06 00:01:00",
+        "2018-06-06 00:02:00",
+        "2018-06-06 00:03:00",
+        "2018-06-06 00:04:00 ",
+        "2018-06-06 00:05:00",
+        "2018-06-06 00:06:00 ",
+        "2018-06-06 00:07:00 ",
+        "2018-06-06 00:08:00 ",
+        "2018-06-06 00:09:00 ",
+    ]
+
+    data1 = [
+        [0.0, 0.0, 10.0],
+        [2.0, 0.0, 10.0],
+        [0.0, 1.0, 12.0],
+        [-2.0, 2.0, 16.0],
+        [2.0, 3.0, 4.0],
+        [4.0, 4.0, -4.0],
+        [0.0, 5.0, 16.0],
+        [-4.0, 6.0, 40.0],
+        [-2.0, 7.0, 26.0],
+        [0.0, 8.0, 10.0],
+        [0.0, 9.0, 10.0],
+    ]
+
+    data2 = [
+        [0.0, 0.0, 10.0],
+        [2.0, 10.0, -10.0],
+        [0.0, 9.0, 8.0],
+        [-2.0, 8.0, 24.0],
+        [2.0, 7.0, -4.0],
+        [4.0, 6.0, -16.0],
+        [0.0, 5.0, 4.0],
+        [-4.0, 4.0, 20.0],
+        [-2.0, 3.0, 14.0],
+        [0.0, 2.0, 10.0],
+        [0.0, 1.0, 10.0],
+    ]
+
+    data = [data1, data2]
+
+    for (position, d) in zip(portfolio.positions, data):
+
+        expected = pd.DataFrame(
+            index=pd.to_datetime(index),
+            data=d,
+            columns=["amount", "price", "principal"],
+        )
+        assert ((expected == position).all()).all()
---
+++ tests/portfolio/test_strategy.py
@@ -0,0 +1,99 @@
+from backlight.portfolio.strategy import generate_simple_trades as module
+import pytest
+import pandas as pd
+import numpy as np
+
+import backlight
+
+
+@pytest.fixture
+def strategy_name():
+    return "simple_entry_and_exit"
+
+
+@pytest.fixture
+def strategy_params():
+    return {"max_holding_time": pd.Timedelta("2min")}
+
+
+@pytest.fixture
+def signals():
+    signals = []
+
+    symbol = "usdjpy"
+    periods = 10
+    df = pd.DataFrame(
+        index=pd.date_range(start="2018-06-06", freq="1min", periods=periods),
+        data=[
+            [1, 0, 0],
+            [0, 0, 1],
+            [0, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+            [0, 0, 1],
+            [0, 0, 1],
+            [0, 0, 0],
+            [0, 0, 0],
+            [1, 0, 0],
+        ],
+        columns=["up", "neutral", "down"],
+    )
+    signals.append(backlight.signal.from_dataframe(df, symbol))
+
+    symbol = "eurjpy"
+    periods = 10
+    df = pd.DataFrame(
+        index=pd.date_range(start="2018-06-06", freq="1min", periods=periods),
+        data=[
+            [1, 0, 0],
+            [0, 0, 1],
+            [0, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+            [0, 0, 1],
+            [0, 0, 1],
+            [0, 0, 0],
+            [0, 0, 0],
+            [1, 0, 0],
+        ],
+        columns=["up", "neutral", "down"],
+    )
+    signals.append(backlight.signal.from_dataframe(df, symbol))
+
+    return signals
+
+
+@pytest.fixture
+def markets():
+    markets = []
+    symbol = "usdjpy"
+    periods = 10
+    df = pd.DataFrame(
+        index=pd.date_range(start="2018-06-06", freq="1min", periods=periods),
+        data=np.arange(periods)[:, None],
+        columns=["mid"],
+    )
+    markets.append(backlight.datasource.from_dataframe(df, symbol))
+
+    symbol = "eurjpy"
+    periods = 10
+    df = pd.DataFrame(
+        index=pd.date_range(start="2018-06-06", freq="1min", periods=periods),
+        data=10 - np.arange(periods)[:, None],
+        columns=["mid"],
+    )
+    markets.append(backlight.datasource.from_dataframe(df, symbol))
+    return markets
+
+
+def test_generate_simple_trades(markets, signals, strategy_name, strategy_params):
+    generated_trades = module(markets, signals, strategy_name, strategy_params)
+
+    expected = pd.DataFrame(
+        index=markets[0].index,
+        data=[1, -1, -1, 2, 1, -2, -2, 1, 1, 0],
+        columns=["amount"],
+    )
+
+    for trades in generated_trades:
+        assert (trades.amount == expected.amount).all()
