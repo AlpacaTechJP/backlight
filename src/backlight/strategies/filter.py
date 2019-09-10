b731029a35df9c28a18d76b326547a668bbc774c
keisuke umezawa <keisuke-umezawa@users.noreply.github.com>
2019-09-10 13:56:17 +0900

    Implement skip entry by hours (#114)
    * Implement skip entry by hours
    
    * Fix mypy
    
    * Fix mypy
    
    * Update version

--- src/backlight/strategies/filter.py
+++ src/backlight/strategies/filter.py
@@ -61,9 +61,7 @@ def skip_entry_by_spread(
     return trades[~trades["_id"].isin(deleted_ids)]
 
 
-def filter_entry_by_time(
-    trades: Trades, unit: str, container_set: tuple
-) -> Type["Trades"]:
+def filter_entry_by_time(trades: Trades, unit: str, container_set: tuple) -> Trades:
     """Filter trade which match conditions at least one element. 
         -> e.g. for container_set = [1,2] and unit = 'hour' Trades of hour 1 or 2 
             will be return.
@@ -104,3 +102,21 @@ def filter_entry_by_time(
     df.index.name = None
 
     return from_dataframe(df, trades.symbol, trades.currency_unit)
+
+
+def skip_entry_by_hours(trades: Trades, hours: List[int]) -> Trades:
+    """Skip entry by hours.
+
+    Args:
+        trades: Trades
+        hours: Hours which will be filtered out from entry.
+    Result:
+        Trades
+    """
+    deleted_ids = []
+    for i in trades.ids:
+        entry = trades.get_trade(i).index[0]
+        if entry.hour in hours:
+            deleted_ids.append(i)
+
+    return trades[~trades["_id"].isin(deleted_ids)]
