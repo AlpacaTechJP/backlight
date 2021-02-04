32ad8cdb51ce6509a0f51f48953f755219edc028
Pivot19 <pivot19@gmail.com>
2021-02-05 01:04:42 +0900

    Add MarketCloseAwareDynamicNeutral

--- src/backlight/labelizer/ternary/dynamic_neutral.py
+++ src/backlight/labelizer/ternary/dynamic_neutral.py
@@ -46,3 +46,33 @@ class DynamicNeutralLabelizer(Labelizer):
     @property
     def neutral_hard_limit(self) -> str:
         return self._params["neutral_hard_limit"]
+
+
+class MarketCloseAwareDynamicNeutralLabelizer(DynamicNeutralLabelizer):
+    def _calculate_dynamic_neutral_range(self, diff_abs: pd.Series) -> pd.Series:
+
+        df = pd.DataFrame(diff_abs, columns=["res"])
+        df.loc[:, "est"] = df.index.tz_convert("America/New_York")
+        freq = int(
+            pd.Timedelta(self._params["neutral_window"])
+            / pd.Timedelta(diff_abs.index.freq)
+        )
+
+        mask = (
+            ~((df.est.dt.hour <= 17) & (df.est.dt.dayofweek == 6))
+            & ((df.est.dt.hour < 16) | (df.est.dt.hour > 17))
+            & ~((df.est.dt.hour >= 16) & (df.est.dt.dayofweek == 4))
+            & (df.est.dt.dayofweek != 5)
+        )
+
+        dnr = (
+            df.loc[mask, "res"]
+            .rolling(freq)
+            .quantile(self.neutral_ratio)
+            .reindex(diff_abs.index)
+            .fillna(100)
+        )
+
+        dnr[dnr < self.neutral_hard_limit] = self.neutral_hard_limit
+
+        return dnr
